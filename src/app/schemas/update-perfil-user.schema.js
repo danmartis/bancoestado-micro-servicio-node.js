8423727b35561934fbd5a7ec43bc18b09299e23b
jgamboa3 <jgamboa3@externos.bancoestado.cl>
2020-03-27 12:27:58 -0300

    add test update user

--- src/app/schemas/update-perfil-user.schema.js
+++ src/app/schemas/update-perfil-user.schema.js
@@ -4,13 +4,7 @@ import { date, phone } from "./types";
 export const UpdatePerfilUserSchema = Joi.object({
   birthday: date,
   phone: phone,
-  address: Joi.string()
-    .required()
-    .max(150),
-  comuna: Joi.string()
-    .required()
-    .max(50),
-  city: Joi.string()
-    .required()
-    .max(50)
+  address: Joi.string().required().max(150),
+  comuna: Joi.string().required().max(50),
+  city: Joi.string().required().max(50)
 });
